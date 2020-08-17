6f3ce4c8b8fb5552ee32c9796a5725f5979a7533
danielmonk <danielmonk@msn.com>
2020-08-17 10:32:53 +0100

    adding text block

---
+++ src/components/TextBlock.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Grid, Cell } from 'react-foundation';
+
+export default class TextBlock extends Component {
+  render() {
+    return (
+    <section className="text-block">
+      <Grid className="grid-container">
+        <Grid className="display align-middle align-center">
+          <Cell small={12} large={10}>
+              <h3>{this.props.heading}</h3>
+              <p>{this.props.content}</p>
+          </Cell>
+        </Grid>
+        </Grid>
+      </section>
+    );
+  }
+}
